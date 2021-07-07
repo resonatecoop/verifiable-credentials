d96016c8e5695e26a21f7fc22108bbd7b46f2862
angusmcleod <angus@mcleod.org.au>
2021-07-07 08:50:23 +0800

    Update user.js

--- vc-app/routes/user.js
+++ vc-app/routes/user.js
@@ -1,40 +1,81 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database');
+var User = db.User;
 
-router.get('/authenticate', function(request, response) {
-  // validate username and OTP
+router.post('/authenticate', async function(request, response) {
+  const otp = request.body.otp;
+  const username = request.body.username;
 
-  response.end();
+  if (!otp || !username) {
+    response.status('401');
+    return response.send('Authentication Failure');
+  }
+
+  const user = await User.findOne({
+    where: {
+      otp,
+      username
+    }
+  });
+
+  if (user) {
+    response.status(200);
+    response.contentType('json');
+    response.send({
+      username: user.username,
+      userHandler: user.uuid
+    })
+  } else {
+    response.status('401');
+    response.send('Authentication Failure');
+  }
 });
 
-router.get('/getAllAttrs/:username', function(request, response) {
+router.get('/getAllAttrs/:uuid', async function(request, response) {
+  const uuid = request.params.uuid;
 
-  // User API returns requested user attributes to VC Layer 
-  const vc_context = "https://resonate.is/VCcontext/v1";
-  const vc_type = "SupporterCredential";
-  const user = {}
-  const artist_data = {
-    "name": "Kallie Marie"
-  };
+  if (!uuid) {
+    response.status('403');
+    return response.send('No user found');
+  }
 
-  response.setHeader('Content-Type', 'application/json');
-  response.end(
-    JSON.stringify(
-      [{
+  let user = await User.findOne({
+    where: {
+      uuid
+    }
+  });
+
+  if (user) {
+    const context = "https://resonate.is/VCcontext/v1";
+    const type = "SupporterCredential";
+    const artist = {
+      "name": "Kallie Marie"
+    };
+    const credentials = [
+      {
         "@context": [
           "https://www.w3.org/2018/credentials/v1",
           "https://schema.org/",
-          vc_context
+          context
         ],
         "type": [
           "VerifiableCredential",
-          vc_type
+          type
         ],
-        "name": user.name,
-        "artist": artist_data
-      }]
-    )
-  );
+        "id": user.uuid,
+        "name": user.username,
+        "artist": artist
+      }
+    ];
+
+    response.status(200);
+    response.contentType('json');
+    response.send(JSON.stringify(credentials));
+  } else {
+    response.status('403');
+    response.send('No user found');
+  }
 });
 
 module.exports = router;
